2d46cb0776a5ff1f6adbf163dd0f4860fa66d65a
Kyle Vu <ktvu@hbuhsd.edu>
2025-02-19 15:32:15 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,4 +13,16 @@ public class Main
       System.out.println("infinite loop! replace with your code");
     }
   }
+
+  // Problem 2 - HighestNum
+  public static Integer highestNum(ArrayList<Integer> arr)
+  {
+    return null;
+  }
+
+  // Problem 3 - getEvens
+  public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
+  {
+    return null;
+  }
 }
